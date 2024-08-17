574040dd8bec4351ae241a8beaaa16b51df3563a
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-08-17 17:37:37 +0530

    Update Comments.jsx

--- Comments.jsx
+++ Comments.jsx
@@ -1,1 +1,77 @@
-ad
+import {useState} from 'react';
+
+export default function Comments(){
+
+let [formData, setformData]= useState({
+  
+remark: "",
+rating: "5",
+});
+
+
+let handleInputChange = (event) => {
+setformData((currData) => {
+return {...currData, [event.target.name]: event.target.value };
+
+});
+};
+
+let handleSubmit = (event) => {
+
+console.log(formData); 
+event.preventDefault();
+useState({
+remark: "",
+rating: "",
+});
+};
+
+  
+return (
+
+<div>
+<h4>Give a comment!</h4>
+
+<form onSubmit={handleSubmit}>
+  
+  <label htmlFor="remark">Remarks</label>
+  <textarea
+  value={formData.remark}
+  placeholder="add few remarks"
+  onChange={handleInputChange}
+  id="remark"
+  name="remark"
+  />
+  <br></br> <br></br>
+
+  <label htmlfor="rating">Rating</label>
+  <input
+    type="number"
+    value={formData.rating}
+    placeholder="rate from 1 to 5"
+    min="1"
+    max="5"
+    onChange={handleInputChange}
+    id="rating"
+    name="rating"/>
+  <br></br> <br></br>
+  <button>Submit</button>
+
+
+
+</form>
+</div>
+);
+
+  
+
+
+
+
+
+
+
+
+
+
+
